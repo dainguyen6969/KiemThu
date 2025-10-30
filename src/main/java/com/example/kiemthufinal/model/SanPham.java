e2dbfb2e40e7ad641cd972adfa33f4b32d4877e2
dainguyen6969 <daintth03468@fpt.edu.vn>
2025-10-30 19:18:50 +0700

    thay doi

--- src/main/java/com/example/kiemthufinal/model/SanPham.java
+++ src/main/java/com/example/kiemthufinal/model/SanPham.java
@@ -1,12 +1,5 @@
 package com.example.kiemthufinal.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class SanPham {
     private String ma;
     private String ten;
@@ -14,4 +7,64 @@ public class SanPham {
     private Float gia;
     private Integer soLuong;
     private String danhMuc;
+
+    public SanPham(String ma, String ten, Integer namBaoHanh, Float gia, Integer soLuong, String danhMuc) {
+        this.ma = ma;
+        this.ten = ten;
+        this.namBaoHanh = namBaoHanh;
+        this.gia = gia;
+        this.soLuong = soLuong;
+        this.danhMuc = danhMuc;
+    }
+
+    public SanPham() {
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public Integer getNamBaoHanh() {
+        return namBaoHanh;
+    }
+
+    public void setNamBaoHanh(Integer namBaoHanh) {
+        this.namBaoHanh = namBaoHanh;
+    }
+
+    public Float getGia() {
+        return gia;
+    }
+
+    public void setGia(Float gia) {
+        this.gia = gia;
+    }
+
+    public Integer getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(Integer soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public String getDanhMuc() {
+        return danhMuc;
+    }
+
+    public void setDanhMuc(String danhMuc) {
+        this.danhMuc = danhMuc;
+    }
 }
